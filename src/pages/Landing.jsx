fbd322138d27256b19012e10015f3ff039218103
judgeOstrich <jeraldnyaga15@gmail.com>
2025-07-25 16:07:52 +0300

    Fix home button routing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Container from "../components/Container";
-import About from "../components/About"
-import Working from "../components/Working"
+import About from "../components/About";
+import Working from "../components/Working";
 import Footer from "../components/Footer";
 
 const Landing = () => {
   return (
     <>
+    <div id ="home" className="min-h-[0px]"></div>
       <Header />
       <Container />
       <About />
