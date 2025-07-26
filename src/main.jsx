c8ddba6d13e295556cd2f1a3c9ebe4d591add5d2
judgeOstrich <jeraldnyaga15@gmail.com>
2025-07-26 15:40:34 +0300

    Add report form route

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import "@fontsource/inter";
 import "@fontsource/roboto";
 import './index.css'
