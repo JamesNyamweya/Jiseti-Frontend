ae0c5f7cd56833275b9e55d5b2a371177c3832e2
judgeOstrich <jeraldnyaga15@gmail.com>
2025-07-28 11:36:37 +0300

    Remove redundant paths and secure paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Landing from "./pages/Landing";
-import Login from "./pages/Login"
-import Signup from "./pages/SignUp"
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import React from "react";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
 import UserDash from "./pages/UserDash";
-import ReportForm from "./pages/ReportForm";
-import AdminDashboard from "./pages/AdminDashboard";
-import Dashboard from "./components/UserDashTable";
-import Logout from "./pages/Logout"
+import AdminDash from "./pages/Admin";
+import Logout from "./pages/Logout";
 // import CreateRecord from "./components/CreateRecord";
 // import EditRecord from "./components/EditRecord";
 
@@ -18,13 +18,25 @@ function App() {
       <Toaster position="top-center" />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/user_dash" element={<UserDash />} />
-        <Route path="/report_form" element={<ReportForm />} />
-        <Route path="/admin_dashboard" element={<AdminDashboard  />} />
-        <Route path="/report" element={<ReportForm/>} />
+        <Route path="/admin" element={<AdminDash />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dash" element={<Dashboard />}/>
+        <Route
+          path="/user_dash"
+          element={
+            <ProtectedRoute>
+              <UserDash />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <AdminRoute>
+              <AdminDash />
+            </AdminRoute>
+          }
+        />
         <Route path="/logout" element={<Logout />} />
       </Routes>
     </Router>
